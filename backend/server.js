ba9b120bd4cd14de63a2cc985074abbbd273b7d5
diagui <tounkarakan@gmail.com>
2024-10-15 00:11:41 +0000

    Week 4 ---> Test - Backend is Ready for testing

--- backend/server.js
+++ backend/server.js
@@ -36,11 +36,11 @@ app.use('/api/auth/budget', budgetRoutes);
 app.all('*',(req, res) => {
     res.status(404)
     if(req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views' ,'404.html'))
+        res.sendFile(path.join(__dirname, 'views' ,'customPage.html'))
     } else if (req.accepts('json')){
-        res.json({message: '404 Not found'})
+        res.json({message: 'Le Backend est Prêt'})
     } else {
-        res.type('txt').send('404 Not found')
+        res.type('txt').send('Le Backend est Prêt')
     }
 });
 
