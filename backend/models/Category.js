6cb721237a22dc601979ce708e9489d020cc2081
TOUNKARA Diagui <101643951+tounkaradiagui@users.noreply.github.com>
2024-10-25 13:48:12 +0300

    Update Model Category.js
    Compound unique index on userId and category name.

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 
 const CategorySchema = new mongoose.Schema({
-    name: {type: String, unique: true},
+    name: {type: String, required: true},
     description: {type: String},
     userId: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: false},
 }, { collection: "Category", timestamps: true });
+
+CategorySchema.index({ userId: 1, name: 1 }, { unique: true });
+
 const Category = mongoose.model("Category", CategorySchema);
 
 module.exports = Category;
